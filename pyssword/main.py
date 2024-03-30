15797915173089d697e6c00b623e333dbde44e3b
Rodrigo Drumond Valente <100377010+rodrigorxd@users.noreply.github.com>
2024-03-30 16:27:32 -0300

    Update main.py

--- pyssword/main.py
+++ pyssword/main.py
@@ -8,17 +8,14 @@ letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","
 numbers = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
 symbols = ["!", "@", "#", "$", "%", "&", "*"]
 
-password = []
-
 finish = False
 
 while not finish:
     art.menuPrincipal()
+    #Reset password     
+    password = []
     #menu
     option = int(input("1 - Password Generator\n2 - Check password leak\n3 - Quit\n\nChoose an option:\n"))
-    qt_letters = 0
-    qt_numbers = 0
-    qt_symbols = 0
     match option:
         #Password Generator - Gerador de senha
         case 1:
