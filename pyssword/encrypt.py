a51e4ac3bfc5da8e8274ae5c5878daf4645ce8dc
Rodrigo Drumond Valente <100377010+rodrigorxd@users.noreply.github.com>
2024-07-13 01:14:38 -0300

    Update encrypt.py

--- pyssword/encrypt.py
+++ pyssword/encrypt.py
@@ -1,21 +1,17 @@
 import hashlib
 
-#encrypt input password - SHA1
+# encrypt input password - SHA1
 def encrypt_password(pass_check):
     pass_encrypt = hashlib.sha1()
     pass_encrypt.update(pass_check.encode())
     return pass_encrypt.hexdigest()
 
-#store first 5 encrypted digits
+# store first 5 encrypted digits
 def first_five(encrypted):
-    five_digits = ""
-    for i in range(0, 5):
-        five_digits += encrypted[i]
+    five_digits = encrypted[0:5]
     return five_digits
 
-#store other encrypted digits
+# store other encrypted digits
 def second_part(encrypted):
-    other_digits = ""
-    for i in range(5, len(encrypted)):
-        other_digits += encrypted[i]
+    other_digits = encrypted[5:len(encrypted)]
     return other_digits
